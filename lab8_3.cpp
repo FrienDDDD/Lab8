d32e38f2c7a2c2830dc77f5d62ca87628d02f2b0
FrienDDDD <friendjunk@gmail.com>
2021-12-03 13:29:53 +0700

    Lab8_3.cpp complete

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -2,7 +2,24 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	char a[26] = {'A','B','C','D','E','F','G','H','I','G','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+	int i = 0 ;
+	while(i < 26){
+		if( x == 'A'){
+			return a[25] ;
+		}
+		if(a[i] == x){
+			return a[i-1] ;
+		}else{
+			i ++ ;
+		}
+	}
+	if( i == 26){
+		return '0' ;
+	}
+	
+	return 0 ;
+
 }
 
 int main(){
@@ -15,5 +32,7 @@ int main(){
 	cout << before('a') << "\n";
 	cout << before('0') << "\n";
 	cout << before('c') << "\n";
+	
 	return 0;
+
 }
